293202f25cb73027f4614e988a32d1abcb4069b9
anastasykirsanova99-create <anastasykirsanova99@gmail.com>
2026-05-22 15:01:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from datetime import datetime, timedelta
+import json
+import os
 
 app = Flask(__name__)
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-import json
-import os
-
+# Google credentials из Render Environment
 info = json.loads(os.environ["GOOGLE_CREDENTIALS"])
 
 credentials = service_account.Credentials.from_service_account_info(
@@ -31,31 +32,23 @@ def create_event():
     date = data.get('date')
     time = data.get('time')
 
+    # start time
+    start_dt = datetime.fromisoformat(f"{date}T{time}:00")
+    end_dt = start_dt + timedelta(hours=1)
+
     event = {
         'summary': f'{service_name} - {name}',
         'description': f'Телефон: {phone}',
         'start': {
-            'dateTime': f'{date}T{time}:00',
+            'dateTime': start_dt.isoformat(),
             'timeZone': 'Europe/Kyiv',
         },
-        from datetime import datetime, timedelta
+        'end': {
+            'dateTime': end_dt.isoformat(),
+            'timeZone': 'Europe/Kyiv',
+        },
+    }
 
-start_dt = datetime.fromisoformat(f"{date}T{time}:00")
-end_dt = start_dt + timedelta(hours=1)
-
-event = {
-    'summary': f'{service_name} - {name}',
-    'description': f'Телефон: {phone}',
-    'start': {
-        'dateTime': start_dt.isoformat(),
-        'timeZone': 'Europe/Kyiv',
-    },
-    'end': {
-        'dateTime': end_dt.isoformat(),
-        'timeZone': 'Europe/Kyiv',
-    },
-  },
-}
     service.events().insert(
         calendarId=CALENDAR_ID,
         body=event
@@ -65,4 +58,4 @@ event = {
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=10000)
